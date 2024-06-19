e271e594fcfb3c0259e351cff194524e78e066d5
Butxer93 <151557284+Butxer93@users.noreply.github.com>
2024-06-19 14:42:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Cree una clase de Python llamada Usuario que use el método init y cree un nombre de usuario y una contraseña. 
+
+class Usuario:
+    def __init__(self, nombre_usuario, contraseña):
+        self.nombre_usuario = nombre_usuario
+        self.contraseña = contraseña
+
+    def __str__(self):
+        return f'Usuario: {self.nombre_usuario}'
+
+
+
+# Crea un objeto usando la clase:
+
+usuario1 = Usuario('ibon93', 'contraseñaSegura')
